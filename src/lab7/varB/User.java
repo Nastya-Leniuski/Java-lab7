c57b620353c05b80824661d1fb033426154b6efa
Nastya Leniuski <nastyamaseik@gmail.com>
2020-06-09 11:56:06 +0200

    lab7

---
+++ src/lab7/varB/User.java
@@ -0,0 +1,21 @@
+package lab7.varB;
+
+/*Клоасс - тип данных "Пользователь" с функциями*/
+public class User {
+
+    private String name;
+    private String addres;
+
+    public User (String name, String addres) {
+        this.name = name;
+        this.addres = addres;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+}
